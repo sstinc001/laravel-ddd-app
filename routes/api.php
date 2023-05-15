09762f5e0c3d613988ed12787c5777ba1a316a71
森紀洋 <n.mori@unified.jp>
2023-05-15 22:00:26 +0900

    refs #23 @3h #react->vue変更

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::namespace('Api')
+    ->name('api.')
+    ->group(function () {
+
+        // region api version 1
+        Route::pattern('apiVersion1', 'v[1]');
+        Route::namespace('V1')
+            ->name('v1.')
+            ->prefix('{apiVersion1}')
+            ->group(function () {
+
+                // region auth
+                Route::prefix('auth')
+                    ->name('auth.')
+                    ->group(function () {
+
+                        Route::post('register', [\App\Http\Controllers\Api\V1\Auth\RegisterController::class, 'index'])->name('register');
+                        Route::post('login', [\App\Http\Controllers\Api\V1\Auth\LoginController::class, 'index'])->name('login');
+                        Route::post('logout', [\App\Http\Controllers\Api\V1\Auth\LogoutController::class, 'index'])->name('logout');
+
+                    });
+                // endregion auth
+
+            });
+        // endregion api version 1
+
+        // region api version all
+        Route::pattern('apiVersionAll', 'v[0-9]+');
+        Route::namespace('V0')
+            ->name('v0.')
+            ->prefix('{apiVersionAll}')
+            ->group(function () {
+
+                // region test
+                Route::prefix('test')
+                    ->name('test.')
+                    ->group(function () {
+                        Route::match(['get', 'post'], '/', [\App\Http\Controllers\Api\V0\Test\TestController::class, 'index'])->name('index');
+                    });
+                // endregion test
+            });
+        // endregion api version all
+
+    });
